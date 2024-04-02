50044aa86be0f407799ee4236770ca828aadf1bc
ycy <291492250@qq.com>
2024-04-02 15:07:37 +0800

    跨域，背景图片

---
+++ web/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <div>hello world</div>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+</script>
+
+<style>
+body {
+  background-image: url("@/assets/background.png");
+  background-size: cover;
+}
+</style>
